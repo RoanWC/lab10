91df6500203190a2fd245793972a87244bc3698a
RoanWC <Roanwc@gmail.com>
2017-04-07 14:36:08 -0400

    finished the project

---
+++ Lab10/Lab10/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Lab10.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab10.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            using(var db = new Entities())
+            {
+                var contactQuery = (from Address in db.Addresses orderby Address.LastName, Address.FirstName select Address).ToList<Address>();
+                return View(contactQuery);
+
+            }
+        }
+    }
+}
